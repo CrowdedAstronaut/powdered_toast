ff705c5479cd89dd0671939f23b5ef3c63dd0e96
CrowdedAstronaut <ross@johnrossphillips.com>
2022-02-15 12:23:54 -0600

    global scss variables are working

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-console.log("Hello from the scripts file!");
+let lastScrollpos = window.pageYOffset;
+let menuButtonCheck = document.querySelector(".menu-btn");
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (lastScrollpos > currentScrollPos) {
+    document.getElementById("top-nav").style.top = "0";
+  } else {
+    document.getElementById("top-nav").style.top = "-75px";
+  }
+  lastScrollpos = currentScrollPos;
+  menuButtonCheck.checked = false;
+};
